1431c578618ece5b711a692fc3282832e9437175
jonathan <hokitjonathan@Hos-MacBook-Air-2.local>
2024-03-19 22:24:23 +0800

    added get and post route

--- src/handlers.ts
+++ src/handlers.ts
@@ -1,15 +1,54 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import AWS from "aws-sdk";
+import { v4 } from "uuid";
 
-export const hello = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+const docClient = new AWS.DynamoDB.DocumentClient();
+const tableName = "ProductsTable";
+
+// Post a new product
+export const createProduct = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  const reqBody = JSON.parse(event.body as string);
+
+  const product = {
+    ...reqBody,
+    productID: v4(),
+  };
+
+  await docClient
+    .put({
+      TableName: tableName,
+      Item: product,
+    })
+    .promise();
+
+  return {
+    statusCode: 201,
+    body: JSON.stringify(product),
+  };
+};
+
+// Get a product by ID
+export const getProduct = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  const id = event.pathParameters?.id;
+  const output = await docClient
+    .get({
+      TableName: tableName,
+      Key: {
+        productID: id,
+      },
+    })
+    .promise();
+
+  if (!output.Item) {
+    return {
+      statusCode: 404,
+      body: JSON.stringify({
+        error: "Not Found",
+      }),
+    };
+  }
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Go Serverless v1.0! Your function executed successfully!",
-        input: event,
-      },
-      null,
-      2,
-    ),
+    body: JSON.stringify(output?.Item),
   };
 };
